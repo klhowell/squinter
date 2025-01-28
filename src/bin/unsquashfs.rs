2bba573edc7424e7f2e4fc0ed28cb7bccb725a2a
Kyle Howell <kyle@howellweb.net>
2025-01-27 22:16:27 -0500

    Fix path filtering and make output match reference unsquashfs

--- src/bin/unsquashfs.rs
+++ src/bin/unsquashfs.rs
@@ -28,31 +28,38 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn list_filesystem(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
-    let default_file_list = &vec![PathBuf::from("/")];
     let mut sqfs = SquashFS::open(&args.filesystem)?;
 
+    let top_dir = PathBuf::from("squashfs-root");
     let file_list = if args.files.is_empty() {
-        &default_file_list
+        vec![top_dir.clone()]
     } else {
-        &args.files
+        args.files.iter()
+            .map(|f| top_dir.join(f.strip_prefix("/").unwrap_or(f)))
+            .collect()
     };
 
-    for f in file_list {
-        for d in sqfs.read_dir(f)? {
-            print_and_descend_dir(&mut sqfs, f, &d)?;
-        }
+    let root_inode = sqfs.root_inode()?;
+    println!("{}", top_dir.to_str().unwrap());
+    for d in sqfs.read_dir_inode(&root_inode)? {
+        print_and_descend_dir(&mut sqfs, &file_list, &top_dir, &d)?;
     }
     Ok(())
 }
 
-fn print_and_descend_dir(sqfs: &mut SquashFS<File>, parent: &Path, d: &DirEntry) -> Result<(), Box<dyn std::error::Error>> {
+fn print_and_descend_dir(sqfs: &mut SquashFS<File>, files: &Vec<PathBuf>, parent: &Path, d: &DirEntry) -> Result<(), Box<dyn std::error::Error>> {
     let path = parent.join(d.file_name());
+
+    if !files.iter().any(|p| path.starts_with(p) || p.starts_with(&path)) {
+        return Ok(());
+    }
+
     println!("{}", path.to_str().unwrap());
 
     let inode = sqfs.inode(d.inode_ref())?;
     if matches!(inode.inode_type, metadata::InodeType::BasicDir) {
         for d in sqfs.read_dir_inode(&inode)? {
-            print_and_descend_dir(sqfs, &path, &d)?;
+            print_and_descend_dir(sqfs, files, &path, &d)?;
         }
     }
     Ok(())
