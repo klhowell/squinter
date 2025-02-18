7e3758746ee9c61ac9b0df70a9ddf87253c394c1
Kyle Howell <kyle@howellweb.net>
2025-02-17 22:55:41 -0500

    Checkpoint; passes test on all compressors with huge speed improvements

--- squinter/src/squashfs/filedata.rs
+++ squinter/src/squashfs/filedata.rs
@@ -3,18 +3,17 @@ use std::io::{Read, Seek, SeekFrom};
 
 use super::block::{CachingReader, FragmentBlockCache, FragmentReader};
 use super::metadata::{self, FragmentEntry, Inode, InodeExtendedInfo};
-use super::readermux::ReaderClient;
+use super::readermux::{ReaderClient, ReaderMux};
 use super::superblock::{Superblock, Compressor};
 use super::compressed::CompressedBlockReader;
 
-#[derive(PartialEq)]
 struct FileBlockInfo<R> {
     disk_offset: u64,
     disk_len: u32,
     data_offset: u32,       // How far into this block the file data starts (only applies to tail-ends)
     data_len: u32,          // Amount of uncompressed file data in this block
     is_compressed: bool,
-    reader: Option<R>,
+    reader: BlockReader<R>,
 }
 
 // TODO: TailEnd support
@@ -22,6 +21,32 @@ struct FileBlockInfo<R> {
 struct TailEnd {
     fragment: FragmentEntry,
     offset: u32,
+}
+
+enum BlockReader<R> {
+    Block(CachingReader<CompressedBlockReader<ReaderClient<R>>>),
+    Fragment(FragmentReader<ReaderClient<CachingReader<CompressedBlockReader<ReaderClient<R>>>>>),
+    None,
+}
+
+impl<R: Read + Seek> Read for BlockReader<R> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        match self {
+            BlockReader::Block(r) => r.read(buf),
+            BlockReader::Fragment(r) => r.read(buf),
+            BlockReader::None => Err(io::Error::from(io::ErrorKind::InvalidData)),
+        }
+    }
+}
+
+impl<R: Seek> Seek for BlockReader<R> {
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
+        match self {
+            BlockReader::Block(r) => r.seek(pos),
+            BlockReader::Fragment(r) => r.seek(pos),
+            BlockReader::None => Err(io::Error::from(io::ErrorKind::InvalidData)),
+        }
+    }
 }
 
 /// Reader for uncompressed file data from a series of compressed blocks and possibly a tail-end.
@@ -35,12 +60,12 @@ struct TailEnd {
 /// The stream position can be calculated from file position of the current block + the amount
 /// already read from that block.
 pub struct FileDataReader<R: Read + Seek> {
-    inner: CompressedBlockReader<R>,
+    inner: ReaderMux<R>,
     pos: u64,
     comp: Compressor,
     block_size: u32,
     file_size: u64,
-    blocks: Vec<FileBlockInfo<FragmentReader<ReaderClient<CachingReader<CompressedBlockReader<ReaderClient<R>>>>>>>,
+    blocks: Vec<FileBlockInfo<R>>,
 }
 
 impl<R: Read + Seek> FileDataReader<R> {
@@ -50,6 +75,7 @@ impl<R: Read + Seek> FileDataReader<R> {
         let pos = 0;
         let comp = sb.compressor;
         let block_size = sb.block_size;
+        let mut inner = ReaderMux::new(inner);
         let mut blocks = Vec::new();
         match &inode.extended_info {
             InodeExtendedInfo::BasicFile(i) => {
@@ -63,19 +89,24 @@ impl<R: Read + Seek> FileDataReader<R> {
                     } else {
                         remaining as u32
                     };
+                    let is_compressed = (b & 0x1000000) == 0;
+                    let c = if is_compressed { comp } else { Compressor::None };
+                    let disk_len = b & 0xFFFFFF;
+                    let mut client_reader = inner.client();
+                    client_reader.seek(SeekFrom::Start(offset))?;
                     blocks.push( FileBlockInfo {
                         disk_offset: offset,
-                        disk_len: b & 0xFFFFFF,
+                        disk_len,
                         data_offset: 0,
-                        data_len: data_len,
-                        is_compressed: (b & 0x1000000) == 0,
-                        reader: None,
+                        data_len,
+                        is_compressed,
+                        reader: BlockReader::Block(CachingReader::new_with_capacity(CompressedBlockReader::new(client_reader,c, disk_len.into(), data_len.into())?, block_size.try_into().unwrap())),
                     });
                     offset += u64::from(*b);
                     remaining -= u64::from(data_len);
                 }
                 if i.frag_index != u32::MAX {
-                    let ft = metadata::FragmentLookupTable::read(md_reader, sb)?;
+                    let ft = metadata::FragmentLookupTable::read(&mut inner.client(), sb)?;
                     let f: &FragmentEntry = &ft.lu_table.entries[i.frag_index as usize];
                     blocks.push( FileBlockInfo {
                         disk_offset: f.start,
@@ -84,11 +115,11 @@ impl<R: Read + Seek> FileDataReader<R> {
                         data_len: i.file_size % block_size,
                         is_compressed: (f.size & 0x1000000) == 0,
                         // Note, block_size is not the uncompressed size; it is the maximum uncompressed size
-                        reader: Some(frag_cache.get_fragment_reader(f.start, (f.size & 0xFFFFFF).into(), block_size.into(), i.block_offset.into(), (i.file_size & block_size).into())?)
+                        reader: BlockReader::Fragment(frag_cache.get_fragment_reader(f.start, (f.size & 0xFFFFFF).into(), block_size.into(), i.block_offset.into(), (i.file_size & block_size).into())?),
                     });
                 }
                 Ok(Some(FileDataReader {
-                    inner: CompressedBlockReader::Base(inner), pos, comp, block_size, file_size, blocks, 
+                    inner, pos, comp, block_size, file_size, blocks, 
                 }))
             }
             _ => Ok(None),
@@ -102,7 +133,7 @@ impl<R: Read + Seek> FileDataReader<R> {
     /** Get the block and (uncompressed) data offset within the block for a given file offset.
      *  Also return the amount of (uncompressed) data left in the block
      */
-    fn calc_block_and_offset(&self, pos: u64) -> Option<(&FileBlockInfo<FragmentReader<ReaderClient<CachingReader<CompressedBlockReader<ReaderClient<R>>>>>>, u32, u32)> {
+    fn calc_block_and_offset(&mut self, pos: u64) -> Option<(&mut FileBlockInfo<R>, u32, u32)> {
         if pos >= self.file_size {
             return None;
         }
@@ -110,9 +141,13 @@ impl<R: Read + Seek> FileDataReader<R> {
         let data_offset = (pos % (self.block_size as u64)) as u32;
 
         let b = &self.blocks[block_index as usize];
-        Some((b, b.data_offset + data_offset, b.data_len - data_offset))
+        let offset = b.data_offset + data_offset;
+        let remaining = b.data_len - data_offset;
+        let b = &mut self.blocks[block_index as usize];
+        Some((b, offset, remaining))
     }
 
+    /*
     /// Do whatever is necessary to make the inner reader's next read come from self.pos
     fn prepare_inner(&mut self) -> io::Result<()> {
         // First close any existing block reader
@@ -141,24 +176,17 @@ impl<R: Read + Seek> FileDataReader<R> {
         } else {
             panic!("start_block: not Base reader");
         }
-    }
+    } */
 }
 
 impl<R: Read + Seek> Read for FileDataReader<R> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        if self.pos >= self.file_size {
-            return Ok(0);
-        }
-        if let CompressedBlockReader::Base(_) = self.inner {
-            self.prepare_inner()?;
-        }
-
-        let mut size = self.inner.read(buf)?;
-        if size == 0 && buf.len() != 0 {
-            // This must be the end of the block. Try to start a new one.
-            self.prepare_inner()?;
-            size = self.inner.read(buf)?;
-        }
+        let (b, offset, _) = match self.calc_block_and_offset(self.pos) {
+            Some(x) => x,
+            None => return Ok(0),
+        };
+        b.reader.seek(SeekFrom::Start(offset.into()))?;
+        let size = b.reader.read(buf)?;
         self.pos += u64::try_from(size).unwrap();
         Ok(size)
     }
@@ -166,33 +194,21 @@ impl<R: Read + Seek> Read for FileDataReader<R> {
 
 impl<R: Read + Seek> Seek for FileDataReader<R> {
     fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
-        let target_pos = match pos {
-            SeekFrom::Start(x) => Ok(x),
-            SeekFrom::End(x) => u64::try_from(x + i64::try_from(self.file_size).unwrap()).map_err(|_| io::Error::from(io::ErrorKind::InvalidInput)),
-            SeekFrom::Current(x) => u64::try_from(x + i64::try_from(self.pos).unwrap()).map_err(|_| io::Error::from(io::ErrorKind::InvalidInput)),
-        }?;
-
-        if matches!(self.inner, CompressedBlockReader::Base(_)) {
-            self.pos = target_pos;
-            return Ok(self.pos);
+        match pos {
+            SeekFrom::Start(p) => {
+                self.pos = p;
+                Ok(self.pos)
+            },
+            SeekFrom::End(p) => {
+                assert!(self.file_size as i64 + p >= 0);
+                self.pos = (self.file_size as i64 + p) as u64;
+                Ok(self.pos)
+            },
+            SeekFrom::Current(p) => {
+                assert!(self.pos as i64 + p >= 0);
+                self.pos = (self.pos as i64 + p) as u64;
+                Ok(self.pos)
+            },
         }
-
-        // If we're able to directly navigate to the desired location within the current reader, do so
-        // Otherwise, Retrieve the base reader, seek it, and read into the new block
-        // Calculate the block and in-block position desired
-        let (cur_block, cur_data_offset, _) = self.calc_block_and_offset(self.pos).unwrap();
-        let (target_block, target_data_offset, _) = self.calc_block_and_offset(target_pos).unwrap();
-
-        if std::ptr::eq(cur_block,target_block) && cur_data_offset <= target_data_offset {
-            // Seeking later in the same block. No need to restart the block. Just read ahead.
-            // TODO: For uncompressed blocks, a fresh seek is probably faster
-            let distance = target_data_offset - cur_data_offset;
-            io::copy(&mut Read::take(&mut self.inner, distance.into()), &mut io::sink())?;
-            self.pos = target_pos;
-        } else {
-            self.pos = target_pos;
-            self.prepare_inner()?;
-        }
-        Ok(self.pos)
     }
 }
